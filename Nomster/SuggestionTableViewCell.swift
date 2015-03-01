82f2f80cbd698a3ee2420d32b925842936c39339
tobias.scholze <tobias.scholze@mediadecision.com>
2015-03-01 16:31:10 +0100

    updated data from ms since 1970 to s since 1970

---
+++ Nomster/SuggestionTableViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  SuggestionTableViewCell.swift
+//  Nomster
+//
+//  Created by Tobias Scholze on 01.03.15.
+//  Copyright (c) 2015 Tobias Scholze. All rights reserved.
+//
+
+import Foundation
